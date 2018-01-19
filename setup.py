0cbd7af19bf7b882d8121665723031d64639d38f
Jordan Gumm <gumm1jn@cmich.edu>
2018-01-19 00:30:01 -0500

    moved to distutils for setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python
-
 from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='nucompress',
       version='0.1',
-      description='Nucleotide Compression Library',
       author='Jordan Gumm',
+      description='Nucleotide Compression Library',
       author_email='gumm@umich.edu',
       packages=['nucompress'],
      )
